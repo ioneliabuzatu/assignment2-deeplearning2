39da336effad8666ac5aa63e58490ec5fefc169f
ioneliabuzatu <buzatuionelia@gmail.com>
2021-04-09 20:26:08 +0200

    [CLUSTER] all_good

---
+++ config.py
@@ -0,0 +1,13 @@
+import experiment_buddy
+
+batch_size = 32
+lr = 0.01
+momentum = 0.9
+
+experiment_buddy.register(locals())
+tensorboard = experiment_buddy.deploy(
+    "mila",
+    sweep_yaml="./sweep.yaml",
+    proc_num=1,
+    wandb_kwargs={"entity": "ionelia"}
+)
