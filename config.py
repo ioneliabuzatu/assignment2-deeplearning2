ffd598536c21c286c22d53a8a1f47550ea34b8fd
ioneliabuzatu <buzatuionelia@gmail.com>
2021-04-09 20:55:45 +0200

    [CLUSTER] test_single_run

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ momentum = 0.9
 experiment_buddy.register(locals())
 tensorboard = experiment_buddy.deploy(
     "mila",
-    sweep_yaml="./sweep.yaml",
+    sweep_yaml="",
     proc_num=1,
     wandb_kwargs={"entity": "ionelia"}
 )
