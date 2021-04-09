c3fb2ad7475331929f1ffc8691e16d2b30210942
ioneliabuzatu <buzatuionelia@gmail.com>
2021-04-09 21:24:03 +0200

    [CLUSTER] try_without_cuda

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import torch
+
 import experiment_buddy
 
 batch_size = 32
@@ -7,7 +9,7 @@ momentum = 0.9
 experiment_buddy.register(locals())
 tensorboard = experiment_buddy.deploy(
     "mila",
-    sweep_yaml="./sweep.yaml",
-    proc_num=1,
+    sweep_yaml="",
+    proc_num=10,
     wandb_kwargs={"entity": "ionelia"}
 )
